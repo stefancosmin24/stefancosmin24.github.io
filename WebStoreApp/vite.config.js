b08ea986f53caeb2b94147fd74173f4ae88e2f25
Stefan Cosmin <SR1588@live.mdx.ac.uk>
2023-03-18 04:29:35 +0000

    added dist

--- WebStoreApp/vite.config.js
+++ WebStoreApp/vite.config.js
@@ -6,7 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/WebStoreApp/index.html",
+  base: "/WebStoreApp/index.html" ,
   plugins: [
     vue2(),
     legacy({
